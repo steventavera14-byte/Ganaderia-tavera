42241ea1584ee7bf119af00e4ec911b0d79bf7c4
steventavera14-byte <steventavera14@icloud.com>
2026-09-14 16:00:35 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Ganadería Tavera",
+  description: "Sistema de Gestión Ganadera",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
